21d40f358d8a29f49de83d66da4b80ca7f3e612d
Elvin Yang <eyy@umich.edu>
2025-10-24 14:05:24 -0400

    Add UR to teleop launch

--- configs/teleop/teleop.launch.py
+++ configs/teleop/teleop.launch.py
@@ -1,28 +1,81 @@
-import launch
-import launch.actions
-import launch.substitutions
 import os
-from ament_index_python.packages import get_package_share_directory
+
+import launch
+import launch.substitutions
 import launch_ros.actions
+from ament_index_python.packages import get_package_share_directory
+from moveit_configs_utils import MoveItConfigsBuilder
+
 
 def generate_launch_description():
-    robot_namespace = launch.substitutions.LaunchConfiguration('namespace', default="")
+    robot_namespace = launch.substitutions.LaunchConfiguration("namespace", default="")
 
-    config = os.path.join(get_package_share_directory('neo_mpo_700-2'),'configs/teleop','teleop.yaml')
-    return launch.LaunchDescription([
-        launch_ros.actions.Node(
-            package='neo_teleop2',
-            executable='neo_teleop2_node',
-            output='screen',
-            name='neo_teleop2_node',
-            namespace=robot_namespace,
-            parameters = [config])
-    ,
-     launch_ros.actions.Node(
-            package='joy', 
-            executable='joy_node', 
-            output='screen',
-            name='joy_node',
-            namespace=robot_namespace,
-            parameters = [{'dev': "/dev/input/js0"}, {'deadzone':0.12}])
-    ])
+    config = os.path.join(
+        get_package_share_directory("neo_mpo_700-2"), "configs/teleop", "teleop.yaml"
+    )
+    ur_teleop_config = os.path.join(
+        get_package_share_directory("neo_teleop2"), "launch", "ur_teleop.yaml"
+    )
+    # TODO(elvout): properly parameterize, see neo_ur_moveit.launch.py
+    moveit_config = (
+        MoveItConfigsBuilder(robot_name="mpo_700", package_name="neo_ur_moveit_config")
+        .robot_description_semantic(
+            file_path=os.path.join(
+                get_package_share_directory("neo_ur_moveit_config"),
+                "srdf",
+                "mpo_700.srdf.xacro",
+            ),
+            mappings={
+                "prefix": "ur10e",
+                "gripper_type": "vg10",
+            },
+        )
+        # TODO(elvout): For some reason this is unable to parse the urdf.
+        # moveit will obtain the urdf from /robot_description with a warning.
+        # .robot_description(
+        #     file_path=urdf_path,
+        #     mappings={
+        #         "arm_type": "ur10e",
+        #         "disable_scanners": True,
+        #         "gripper_type": "vg10",
+        #         "use_ur_dc": True,
+        #         "use_gz": False,
+        #         "force_abs_paths": False,
+        #         "use_mock_hardware": False,
+        #         "mock_sensor_commands": False,
+        #     },
+        # )
+        .to_moveit_configs()
+    )
+
+    return launch.LaunchDescription(
+        [
+            launch_ros.actions.Node(
+                package="neo_teleop2",
+                executable="neo_teleop2_node",
+                output="screen",
+                name="neo_teleop2_node",
+                namespace=robot_namespace,
+                parameters=[config],
+            ),
+            launch_ros.actions.Node(
+                package="neo_teleop2",
+                executable="ur_teleop_node",
+                output="screen",
+                name="ur_teleop_node",
+                parameters=[
+                    ur_teleop_config,
+                    moveit_config.robot_description_semantic,
+                    moveit_config.robot_description_kinematics,
+                ],
+            ),
+            launch_ros.actions.Node(
+                package="joy",
+                executable="joy_node",
+                output="screen",
+                name="joy_node",
+                namespace=robot_namespace,
+                parameters=[{"dev": "/dev/input/js0"}, {"deadzone": 0.12}],
+            ),
+        ]
+    )
